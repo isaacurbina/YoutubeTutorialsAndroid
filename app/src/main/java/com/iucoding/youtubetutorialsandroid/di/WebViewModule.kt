84cdd211437e6fcbe984a2d730b732383da3e936
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-01 19:23:47 -0500

    Injecting ViewModel with parameters (#8)

--- app/src/main/java/com/iucoding/youtubetutorialsandroid/di/WebViewModule.kt
+++ app/src/main/java/com/iucoding/youtubetutorialsandroid/di/WebViewModule.kt
@@ -1,9 +1,8 @@
 package com.iucoding.youtubetutorialsandroid.di
 
 import com.iucoding.youtubetutorialsandroid.ui.screen.webview.WebViewViewModel
-import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val webViewModule = module {
-	viewModelOf(::WebViewViewModel)
+	factory { (url: String) -> WebViewViewModel(url) }
 }
